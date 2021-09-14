812639746e0466c15fc6826807ecbf314070715f
Lindispensable <90699244+Lindispensable@users.noreply.github.com>
2021-09-14 15:03:03 +0200

    Add files via upload

---
+++ public2/html/villes/palluau.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Titre de la page</title>
+        <link rel="stylesheet" href="../../css/base.css">
+        <link rel="stylesheet" href="../../css/header.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <div class="page">
+            <div class="container">
+                <?php include '../navbarTwo.php'; ?>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ public2/html/villes/soullans.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Titre de la page</title>
+        <link rel="stylesheet" href="../../css/base.css">
+        <link rel="stylesheet" href="../../css/header.css">
+        <link rel="stylesheet" href="../../css/ville.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <div class="page">
+            <div class="container">
+                <?php include '../navbarTwo.php'; ?>
+            </div>
+
+            <h1 class="ville__title">Soullans</h1>
+        </div>
+    </body>
+</html>
